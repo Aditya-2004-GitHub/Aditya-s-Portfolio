52d5d441fba06d4a4f4f579606618ad1a0f03c10
Aditya-2004-GitHub <22f3001516@ds.study.iitm.ac.in>
2026-05-07 11:07:10 +0530

    Update - 1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./portfolio/portfolio').then((m) => m.Portfolio),
+  }
+];
